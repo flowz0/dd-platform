fb9d19922fd59ebafa8501f25b9ae05f3bf709cc
flowz0 <billylflowers@gmail.com>
2025-07-26 00:34:50 -0700

    feat: create app page (home) & import (home) components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
+import BeforeAfterSection from "@/components/home/before-after/BeforeAfter";
+import CTASection from "@/components/home/CTA";
+import FAQSection from "@/components/home/faqs/FAQs";
+import Footer from "@/components/home/Footer";
+import Hero from "@/components/home/Hero";
+import Navbar from "@/components/home/Navbar";
+import ReviewsSection from "@/components/home/reviews/Reviews";
+import ServiceAreaSection from "@/components/home/service-area/ServiceArea";
+import ServicesSection from "@/components/home/services/Services";
+import WhyUsSection from "@/components/home/why/WhyUs";
+import WaveDividerIn from "@/components/ui/WaveDividerIn";
+import WaveDividerOut from "@/components/ui/WaveDividerOut";
+
 export default function Page() {
   return (
     <div>
-      <h1>App page</h1>
+      <Navbar />
+      <Hero />
+      <ServicesSection />
+      <WhyUsSection />
+      <WaveDividerIn />
+      <BeforeAfterSection />
+      <WaveDividerOut />
+      <ReviewsSection />
+      <ServiceAreaSection />
+      <FAQSection />
+      <CTASection />
+      <Footer />
     </div>
   );
 }
