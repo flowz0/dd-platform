09356c9306ac8cb121acba9f9fc12d6e7f804784
flowz0 <billylflowers@gmail.com>
2025-07-26 00:24:56 -0700

    fix: update Button component primary variant hover bg color

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -10,7 +10,7 @@ const Button = ({
   const baseStyle = "py-4 px-8 rounded-lg text-p cursor-pointer";
   const variantStyle = {
     text: "text-primary hover:bg-primary/10",
-    primary: "bg-primary text-white95 hover:bg-primary/90",
+    primary: "bg-primary text-white95 hover:bg-[hsl(205,100%,35%)]",
     secondary: "bg-secondary text-white95 hover:bg-[hsl(346,98%,42%)]",
     outlined: "ring ring-primary/60 text-primary hover:ring-primary hover:bg-primary/5"
   }[variant];
