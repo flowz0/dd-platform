2803b6a19b854d6e3dad207d597d063f733b9cc1
flowz0 <billylflowers@gmail.com>
2025-07-27 20:30:04 -0700

    fix: improve BlogFeatures component spacing

--- components/blog/BlogFeatures.tsx
+++ components/blog/BlogFeatures.tsx
@@ -2,22 +2,22 @@ import { FaUser, FaCalendarDays, FaClock } from "react-icons/fa6";
 
 export default function BlogFeatures() {
   return (
-    <section className="flex flex-wrap justify-center items-center gap-x-6 gap-y-4 mt-8 md:gap-x-8">
-      <div className="flex items-center gap-2">
+    <section className="flex flex-wrap justify-center items-center gap-y-6 mt-8 gap-x-12">
+      <div className="flex items-center gap-4">
         <FaUser className="text-primary w-4 h-4" />
-        <h4 className="text-white10 font-semibold">
+        <h4 className="text-white25">
           Karson Kolle
         </h4>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         <FaCalendarDays className="text-[#0080DB] w-4 h-4" />
-        <p className="text-white10 font-semibold">
+        <p className="text-white25">
           July 18, 2025
         </p>
       </div>
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-4">
         <FaClock className="text-[#0080DB] w-4 h-4" />
-        <p className="text-white10 font-semibold">
+        <p className="text-white25">
           3 min read
         </p>
       </div>
