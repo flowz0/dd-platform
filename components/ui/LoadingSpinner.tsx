e7663212cfa9ddb1df03b8d08377b3ae90501e40
flowz0 <billylflowers@gmail.com>
2025-07-29 15:56:42 -0700

    fix: add margin top to LoadingSpinner component

--- components/ui/LoadingSpinner.tsx
+++ components/ui/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 const LoadingSpinner: React.FC = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center mt-32">
       <div className="border-4 border-t-4 border-[#E6E6E6] border-solid w-16 h-16 rounded-full animate-spin border-t-[#0080DB]"></div>
     </div>
   );
