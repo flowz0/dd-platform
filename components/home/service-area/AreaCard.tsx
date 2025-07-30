198b42eb571f9b81e3e8f9a6290e520aa09fdaa9
flowz0 <billylflowers@gmail.com>
2025-07-30 16:34:46 -0700

    fix: add Link to ServiceArea location component

--- components/home/service-area/AreaCard.tsx
+++ components/home/service-area/AreaCard.tsx
@@ -3,9 +3,9 @@ import { FaMapPin } from "react-icons/fa";
 
 export default function AreaCard({ children, className }: AreaCardProps) {
   return (
-    <div className={`${className} flex items-center gap-x-2`}>
+    <div className={`${className} flex items-center gap-x-2 group`}>
       <FaMapPin className="text-secondary h-6 w-6 sm:h-8 sm:w-8" />
-      <p className="text-white10 text-small sm:text-p">
+      <p className="text-white10 text-small group-hover:text-secondary sm:text-p">
         {children}
       </p>
     </div>
--- components/home/service-area/Areas.tsx
+++ components/home/service-area/Areas.tsx
@@ -1,38 +1,59 @@
+import Link from "next/link";
 import AreaCard from "./AreaCard";
 
 export default function Areas() {
   return (
     <div className="grid grid-cols-2 gap-y-6 gap-x-6 sm:grid-cols-3 mt-8">
-      <AreaCard>
-        Oak Grove
-      </AreaCard>
-      <AreaCard>
-        Grain Valley
-      </AreaCard>
-      <AreaCard>
-        Blue Springs
-      </AreaCard>
-      <AreaCard>
-        Lees Summit
-      </AreaCard>
-      <AreaCard>
-        Independence
-      </AreaCard>
-      <AreaCard>
-        Kansas City
-      </AreaCard>
-      <AreaCard>
-        Raymore
-      </AreaCard>
-      <AreaCard>
-        Raytown
-      </AreaCard>
-      <AreaCard>
-        Pleasant Hill
-      </AreaCard>
-      <AreaCard className="sm:hidden">
-        Odessa
-      </AreaCard>
+      <Link href="/service-area/oak-grove">
+        <AreaCard>
+          Oak Grove
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/grain-valley">
+        <AreaCard>
+          Grain Valley
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/blue-springs">
+        <AreaCard>
+          Blue Springs
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/lees-summit">
+        <AreaCard>
+          Lees Summit
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/independence">
+        <AreaCard>
+          Independence
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/kansas-city">
+        <AreaCard>
+          Kansas City
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/raymore">
+        <AreaCard>
+          Raymore
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/raytown">
+        <AreaCard>
+          Raytown
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/pleasant-hill">
+        <AreaCard>
+          Pleasant Hill
+        </AreaCard>
+      </Link>
+      <Link href="/service-area/odessa">
+        <AreaCard className="sm:hidden">
+          Odessa
+        </AreaCard>
+      </Link>
     </div>
   );
 }
