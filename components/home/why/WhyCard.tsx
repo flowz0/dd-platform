8186846f6c781e12dd03b73e86b0cf0973fcbc98
flowz0 <billylflowers@gmail.com>
2025-07-30 23:07:24 -0700

    fix: decrease header margin top in WhyCard component

--- components/home/why/WhyCard.tsx
+++ components/home/why/WhyCard.tsx
@@ -8,7 +8,7 @@ export default function WhyCard({
   return (
     <div className="bg-white90 border border-white75 py-8 px-8 rounded-lg">
       <Icon className="text-primary w-8 h-8 sm:w-12 sm:h-12" />
-      <h6 className="text-white10 text-h6 font-bold mt-8 sm:text-h5">
+      <h6 className="text-white10 text-h6 font-bold mt-6 sm:text-h5">
         {children}
       </h6>
       <p className="text-white25 text-p mt-4">
