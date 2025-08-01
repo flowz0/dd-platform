185960a92d19427ed174dc38284604eff559719e
flowz0 <billylflowers@gmail.com>
2025-07-31 18:00:41 -0700

    fix: disable Reviews link in dashboard Navigation

--- components/navigation/DashboardNavbar.tsx
+++ components/navigation/DashboardNavbar.tsx
@@ -130,6 +130,8 @@ export default function DashboardNavbar() {
               <SidebarLink
                 href="/reviews"
                 label="Reviews"
+                className="cursor-not-allowed"
+                disabled
                 icon={
                   // Outline icon
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
--- components/navigation/Sidebar.tsx
+++ components/navigation/Sidebar.tsx
@@ -90,6 +90,8 @@ export default function Sidebar() {
         <SidebarLink
           href="/reviews"
           label="Reviews"
+          className="cursor-not-allowed"
+          disabled
           icon={
             // Outline icon
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
